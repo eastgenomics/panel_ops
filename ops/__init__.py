e380f5f9f897d1b8be461cbbed32e8eb54c6dd62
Yu-jinKim <yujin.kim@hotmail.fr>
2020-11-06 18:10:15 +0000

    add the missing function

--- ops/__init__.py
+++ ops/__init__.py
@@ -1,16 +1,16 @@
 from .check import (
-    check_gene, check_panelapp_dump_against_db
+    check_gene, check_panelapp_dump_against_db, check_test_against_db
 )
 from .generate import (
     generate_panelapp_dump, generate_genepanels, generate_gms_panels,
     get_all_transcripts, write_django_jsons, get_django_json,
-    create_django_json
+    create_django_json, generate_gemini_names, generate_sample2panels
 )
 # from .update import (
 #     update_django_tables
 # )
 from .utils import (
-    assign_transcript, get_date, connect_to_db, get_GMS_panels, parse_HGMD,
-    get_nirvana_data_dict, create_panelapp_dict, check_if_seen_before,
-    parse_tests_xls, clean_targets
+    assign_transcript, get_date, connect_to_db, get_all_panels, get_GMS_panels,
+    parse_HGMD, get_nirvana_data_dict, create_panelapp_dict, parse_tests_xls,
+    clean_targets, parse_coor, parse_gemini_dump
 )
