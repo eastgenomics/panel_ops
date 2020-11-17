c11908208be26c7c22144d5583455ec40d1cc49e
Yu-jinKim <yujin.kim@hotmail.fr>
2020-11-17 11:55:11 +0000

    init importing of mod_db, logger, queries

--- ops/__init__.py
+++ ops/__init__.py
@@ -1,15 +1,17 @@
 from .check import (
     check_gene, check_panelapp_dump_against_db, check_test_against_db
 )
+from .mod_db import import_django_fixture, update_django_tables
 from .generate import (
     generate_panelapp_dump, generate_genepanels, generate_gms_panels,
     get_all_transcripts, write_django_jsons, get_django_json,
     create_django_json, generate_gemini_names, generate_sample2panels,
     generate_panel_names
 )
-# from .update import (
-#     update_django_tables
-# )
+from .logger import setup_logging
+from .queries import (
+    get_gemini_name, get_genes_from_gemini_name
+)
 from .utils import (
     assign_transcript, get_date, connect_to_db, get_all_panels, get_GMS_panels,
     parse_HGMD, get_nirvana_data_dict, create_panelapp_dict, parse_tests_xls,
