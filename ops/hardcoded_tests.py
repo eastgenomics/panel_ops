ffa8a93595044a1521ec221226d0c9cbb6e37c2b
Yu-jinKim <yujin.kim@hotmail.fr>
2020-11-17 11:56:35 +0000

    docstring to explain why

--- ops/hardcoded_tests.py
+++ ops/hardcoded_tests.py
@@ -1,3 +1,10 @@
+""" Abomination needed for having tests that are not explicit in the test directory
+
+Example:
+R27.1 has Relevant panel in panelapp as a panel.
+Scientists recommendation was to use the DDG2P as a panel.
+"""
+
 tests = {
     "R27.1": {
         "gemini_name": (
