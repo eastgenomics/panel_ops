2bc36560df8e02f201a50dd5d5ff80352591b720
Yu-jinKim <yujin.kim@hotmail.fr>
2020-11-30 15:14:57 +0000

    hardcoded tests

---
+++ ops/hardcoded_tests.py
@@ -0,0 +1,16 @@
+""" Abomination needed for having tests that are not explicit in the test directory
+
+Example:
+R27.1 has Relevant panel in panelapp as a panel.
+Scientists recommendation was to use the DDG2P as a panel.
+"""
+
+tests = {
+    "R27.1": {
+        "gemini_name": (
+            "R27.1a_Congenital malformation and dysmorphism syndromes"
+            " - microarray and sequence (DDG2P)_P"
+        ),
+        "panels": ["484"]
+    }
+}
