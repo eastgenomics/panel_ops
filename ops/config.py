aaad46db35323e64812c22e34d626f2645eebcbf
Yu-jinKim <yujin.kim@hotmail.fr>
2020-12-01 16:57:36 +0000

    removed comma

--- ops/config.py
+++ ops/config.py
@@ -8,5 +8,5 @@ path_to_panel_palace = "/home/egg-user/panels/panel_palace/"
 path_to_panel_config = "/home/egg-user/panels/panel_config"
 
 # Ids for the HGMD pro vcf
-hgmd_dxid = "file-Fv1X0jQ40qBjj4GPKj196kjJ",
+hgmd_dxid = "file-Fv1X0jQ40qBjj4GPKj196kjJ"
 hgmd_project = "project-Fv1Vzkj40qBfq0v38p7q50v8"
