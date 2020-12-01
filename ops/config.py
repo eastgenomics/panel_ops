b5da1a7db98a2f5d56e9da8aa0b17ac70c34922d
Yu-jinKim <yujin.kim@hotmail.fr>
2020-12-01 16:36:01 +0000

    config file

---
+++ ops/config.py
@@ -0,0 +1,12 @@
+#Path to panel logs folder
+path_to_logs = "/home/egg-user/panels/panel_logs"
+
+# Path to the panel_palace folder
+path_to_panel_palace = "/home/egg-user/panels/panel_palace/"
+
+# Path to the panel_config folder
+path_to_panel_config = "/home/egg-user/panels/panel_config"
+
+# Ids for the HGMD pro vcf
+hgmd_dxid = "file-Fv1X0jQ40qBjj4GPKj196kjJ",
+hgmd_project = "project-Fv1Vzkj40qBfq0v38p7q50v8"
