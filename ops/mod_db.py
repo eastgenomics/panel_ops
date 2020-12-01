343dd02796278d7448affa5ee212cec3d15ee099
Yu-jinKim <yujin.kim@hotmail.fr>
2020-12-01 16:32:11 +0000

    docstring + import from config file

--- ops/mod_db.py
+++ ops/mod_db.py
@@ -3,9 +3,10 @@ import sys
 
 import django
 
+from .config import path_to_panel_palace
 from .logger import setup_logging
 
-sys.path.append('/home/kimy/NHS/Panelapp/panel_palace/')
+sys.path.append(path_to_panel_palace)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "panel_palace.settings")
 django.setup()
 
@@ -22,13 +23,12 @@ LOGGER = setup_logging("mod_db")
 
 
 def import_django_fixture(path_to_json):
-    """ Import data to django database using a django fixture (json with specific format)
+    """ Import data to django database using a django fixture (json with
+    specific format)
 
-    Args:
-        path_to_json (str): Path to django fixture
+    Args: path_to_json (str): Path to django fixture
 
-    Returns:
-        bool: True if import works
+    Returns: bool: True if import works
     """
 
     LOGGER.info(f"Importing data using json: '{path_to_json}'")
