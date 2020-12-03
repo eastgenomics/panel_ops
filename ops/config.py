f69a24be412ce829da08321fed5665b9375bcd7e
Yu-jinKim <yujin.kim@hotmail.fr>
2020-12-03 11:13:37 +0000

    test folders

--- ops/config.py
+++ ops/config.py
@@ -1,11 +1,11 @@
 #Path to panel logs folder
-path_to_logs = "/home/egg-user/panels/panel_logs"
+path_to_logs = "/home/kimy/NHS/Panelapp/panel_logs"
 
 # Path to the panel_palace folder
-path_to_panel_palace = "/home/egg-user/panels/panel_palace/"
+path_to_panel_palace = "/home/kimy/NHS/Panelapp/panel_palace/"
 
 # Path to the panel_config folder
-path_to_panel_config = "/home/egg-user/panels/panel_config"
+path_to_panel_config = "/home/kimy/NHS/Panelapp/panel_config"
 
 # Ids for the HGMD pro vcf
 hgmd_dxid = "file-Fv1X0jQ40qBjj4GPKj196kjJ"
