118ca6180488d6db00fd8c505d9e15cbf0f2c7fa
Yu-jinKim <yujin.kim@hotmail.fr>
2020-12-08 09:41:53 +0000

    change name of bio manifest function

--- ops/__init__.py
+++ ops/__init__.py
@@ -5,7 +5,7 @@ from .mod_db import import_django_fixture, update_django_tables
 from .generate import (
     generate_panelapp_dump, generate_genepanels, generate_gms_panels,
     generate_g2t, write_django_jsons, get_django_json,
-    create_django_json, generate_gemini_names, generate_sample2panels,
+    create_django_json, generate_gemini_names, generate_manifest,
     generate_panel_names
 )
 from .logger import setup_logging
