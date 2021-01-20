e5b64f36c2b56864360f245f8d69c48734d7f382
Yu-jinKim <yujin.kim@hotmail.fr>
2021-01-20 22:38:49 +0000

    path config

---
+++ ops/config.py
@@ -0,0 +1,8 @@
+# Path to panel logs folder
+path_to_logs = "/home/kimy/NHS/Panelapp/panel_logs"
+
+# Path to the panel_palace folder
+path_to_panel_palace = "/home/kimy/NHS/Panelapp/panel_palace/"
+
+# Path to the panel_config folder
+path_to_panel_config = "/home/kimy/NHS/Panelapp/panel_config"
