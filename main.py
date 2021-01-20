b730dc884cc8fc44d38991c5b012fe72e0900ae5
Yu-jinKim <yujin.kim@hotmail.fr>
2021-01-20 22:53:10 +0000

    removed some functions, no longer need nirvana gff

--- main.py
+++ main.py
@@ -2,25 +2,16 @@
 
 """ Panel operations
 
-4 subparsers:
+3 subparsers:
 - check:
-    - gene: get transcripts of given gene using the nirvana GFF
     - panel: check the panelapp dump folder given against the database
-    - test: check the National test directory file against the database
 - generate:
     - panelapp_gms: generate panelapp dump using only GMS panels
     - panelapp_all: generate panelapp dump using only all panels
-    - g2t: generate genes2transcripts file + no transcripts file
     - gene_files: generate files for panels with gene symbols only
     - json: generate django fixture using panelapp dump folder
     - genepanels: generate genepanels file
-    - gemini: generate dump of gemini names
     - manifest: generate manifest type file for reports using Gemini db dump
-    - panel_names_gms: generate file with GMS panel names
-    - panel_names_all: generate file with all panel names
-- query:
-    - gemini_name: Query to get the full gemini name given a substring of that name
-    - gene_test: Query to get all the genes for a gemini name
 - mod_db:
     - initial_import: Import given django fixture in the database
 """
@@ -36,48 +27,32 @@ import config_panel_db
 
 
 def main(**param):
-    hgmd_dict = ops.utils.parse_HGMD()
-    nirvana_dict = ops.utils.get_nirvana_data_dict(param["gff"])
-    ci_dict, test_dict = ops.utils.parse_tests_xls(param["test_xls"])
-    test2targets = ops.utils.clean_targets(ci_dict, test_dict)
+    # gather data from the test directory
+    clinind_data = ops.utils.parse_test_directory(param["test_xls"])
+    clean_clinind_data = ops.utils.clean_targets(clinind_data)
+
+    # get the single genes in the test directory
+    single_genes = ops.utils.gather_single_genes(clean_clinind_data)
 
     user = config_panel_db.user_admin
     passwd = config_panel_db.passwd_admin
     host = config_panel_db.host
 
     if param["command"] == "check":
-        # check which transcript given was assigned
-        if param["gene"]:
-            ops.check.check_gene(param["gene"], hgmd_dict, nirvana_dict)
-
         # Check integrity of database for all things panel
-        if param["panel"]:
+        if param["folder"]:
             session, meta = ops.utils.connect_to_db(user, passwd, host)
-            panelapp_dicts = ops.utils.create_panelapp_dict(
-                param["panel"], hgmd_dict, nirvana_dict
+            (
+                panelapp_dict, superpanel_dict, gene_dict
+            ) = ops.utils.create_panelapp_dict(
+                param["folder"], config_panel_db.panel_types, single_genes
             )
-            check = ops.check.check_panelapp_dump_against_db(
-                param["panel"], session, meta, panelapp_dicts
-            )
-
-        # Check integrity of database for tests
-        if param["test"]:
-            session, meta = ops.utils.connect_to_db(user, passwd, host)
-            check = ops.check.check_test_against_db(
-                session, meta, test2targets
+            check = ops.check.check_db(
+                param["folder"], session, meta, panelapp_dict, superpanel_dict,
+                clean_clinind_data
             )
 
     elif param["command"] == "generate":
-        # Generate g2t + genes without transcript files
-        if param["g2t"]:
-            session, meta = ops.utils.connect_to_db(user, passwd, host)
-            ops.generate.generate_g2t(session, meta)
-
-        # Generate gms panel files with only green genes
-        if param["gene_files"]:
-            gms_panels = ops.utils.get_GMS_panels()
-            ops.generate.generate_gms_panels(gms_panels)
-
         # Generate panelapp dump
         if param["panelapp_all"] is True:
             all_panels = ops.utils.get_all_panels()
@@ -92,25 +67,18 @@ def main(**param):
                 gms_panels, "GMS"
             )
 
+        # Generate panelapp dump for non-GMS panels
+        if param["panelapp_non_gms"] is True:
+            non_gms_panels = ops.utils.get_non_GMS_panels()
+            panelapp_dump = ops.generate.generate_panelapp_dump(
+                non_gms_panels, "non_GMS"
+            )
+
         # Generate a bioinformatic manifest type file for reports
         if param["manifest"]:
             session, meta = ops.utils.connect_to_db(user, passwd, host)
             sample2panels = ops.generate.generate_manifest(
                 session, meta, param["manifest"]
-            )
-
-        # Generate file containing all the GMS panels names stored in the db
-        if param["panel_names_gms"]:
-            session, meta = ops.utils.connect_to_db(user, passwd, host)
-            panel_file = ops.generate.generate_panel_names(
-                session, meta, gms=True
-            )
-
-        # Generate file containing all the panels names stored in the db
-        if param["panel_names_all"]:
-            session, meta = ops.utils.connect_to_db(user, passwd, host)
-            panel_file = ops.generate.generate_panel_names(
-                session, meta, gms=False
             )
 
         # Generate django fixture using given panelapp dump
@@ -119,48 +87,67 @@ def main(**param):
             pk_dict = {
                 # they start at 1 because I loop over those elements
                 # makes it easier to keep track off
-                "test": 1, "panel": 1, "reference": 1,
-                "testpanel": 0, "testgene": 0,
-                "panelgene": 0, "panelstr": 0, "panelcnv": 0,
-                "gene": 0, "transcript": 0, "exon": 0,
-                "str": 0, "regionstr": 0,
-                "cnv": 0, "regioncnv": 0,
-                "region": 0, "superpanel": 0
+                "clinind": 1, "panel": 1, "reference": 1,
+                "clinind_panels": 0, "feature": 0, "panel_feature": 0,
+                "gene": 0, "str": 0, "cnv": 0, "panel_type": 1
             }
+
             (
-                panelapp_dict, superpanel_dict, gene_dict,
-                str_dict, cnv_dict, region_dict
+                panelapp_dict, superpanel_dict, gene_dict
             ) = ops.utils.create_panelapp_dict(
-                param["json"], hgmd_dict, nirvana_dict
+                param["json"], config_panel_db.panel_types, single_genes
             )
-            json_lists = ops.generate.create_django_json(
-                ci_dict, test2targets, panelapp_dict, superpanel_dict,
-                gene_dict, str_dict, cnv_dict, region_dict, pk_dict
+
+            # Create the list of reference table
+            reference_json = ops.utils.gather_ref_django_json(
+                config_panel_db.references
             )
-            ops.generate.write_django_jsons(json_lists)
+            # Create the list of panel_type table
+            paneltype_json = ops.utils.gather_panel_types_django_json(
+                config_panel_db.panel_types
+            )
+            # Create the list of feature_type table
+            featuretype_json = ops.utils.gather_feature_types_django_json(
+                config_panel_db.feature_types
+            )
+
+            # Create the list for data associated with panels
+            (
+                panel_json, feature_json, panelfeature_json, featuretype_json,
+                gene_json, pk_dict
+            ) = ops.utils.gather_panel_data_django_json(
+                panelapp_dict, gene_dict, featuretype_json, paneltype_json,
+                reference_json, pk_dict
+            )
+
+            # Add the superpanels to the list of panel objects
+            (
+                panel_json, panelfeature_json
+            ) = ops.utils.gather_superpanel_data_django_json(
+                superpanel_dict, panel_json, paneltype_json, reference_json,
+                panelfeature_json, pk_dict
+            )
+
+            # Create the list of clinical indication
+            (
+                clinical_indication_json, clinical_indication2panels_json
+            ) = ops.utils.gather_clinical_indication_data_django_json(
+                clean_clinind_data, panel_json, pk_dict
+            )
+
+            # Generate the jsons for the import
+            ops.generate.generate_django_jsons(
+                ref=reference_json, panel_type=paneltype_json,
+                feature_type=featuretype_json, panel_feature=panelfeature_json,
+                gene=gene_json, clinical_indication=clinical_indication_json,
+                feature=feature_json, panel=panel_json,
+                clinical_indication2panels=clinical_indication2panels_json
+            )
 
         # Generate genepanels file from database
         if param["genepanels"]:
             session, meta = ops.utils.connect_to_db(user, passwd, host)
             ops.generate.generate_genepanels(session, meta)
-
-        # Generate file containing the name of the tests in Gemini
-        if param["gemini"]:
-            session, meta = ops.utils.connect_to_db(user, passwd, host)
-            ops.generate.generate_gemini_names(session, meta, test2targets)
-
-    elif param["command"] == "query":
-        session, meta = ops.utils.connect_to_db(user, passwd, host)
-
-        # Return the full gemini name given a substring of that name
-        if param["gemini_name"]:
-            ops.queries.get_gemini_name(session, meta, param["gemini_name"])
-
-        # Return the genes given a substring of a gemini name
-        elif param["gene_test"]:
-            ops.queries.get_genes_from_gemini_name(
-                session, meta, param["gene_test"]
-            )
 
     elif param["command"] == "mod_db":
         # Import given django fixture to the database
@@ -177,7 +164,6 @@ if __name__ == "__main__":
 
     subparser = parser.add_subparsers(dest="command")
 
-    parser.add_argument("gff", help="Nirvana GFF dump")
     parser.add_argument("test_xls", help="NHS test directory")
 
     generate = subparser.add_parser("generate")
@@ -186,63 +172,33 @@ if __name__ == "__main__":
         help="Generate panelapp GMS dump"
     )
     generate.add_argument(
+        "-non-gms", "--panelapp_non_gms", action="store_true",
+        help="Generate panelapp non-GMS dump"
+    )
+    generate.add_argument(
         "-all", "--panelapp_all", action="store_true",
         help="Generate all panelapp dump"
     )
     generate.add_argument(
-        "-g2t", "--g2t", action="store_true", help="Genes2transcript file"
-    )
-    generate.add_argument(
-        "-gf", "--gene_files", action="store_true",
-        help="Generate gene files for GMS panels"
-    )
-    generate.add_argument(
-        "-j", "--json",
-        help="Panelapp dump to generate json to import data directly"
+        "-j", "--json", nargs="+",
+        help=(
+            "Dump folders to generate json to import data directly, "
+            "must contain: gms, non_gms, in_house"
+        )
     )
     generate.add_argument(
         "-gp", "--genepanels", action="store_true",
         help="Generate genepanels"
     )
-    generate.add_argument(
-        "-gd", "--gemini", action="store_true",
-        help="Generate dump of database of the gemini names"
-    )
     generate.add_argument("-m", "--manifest", help="Gemini database xls dump")
-    generate.add_argument(
-        "-pn_gms", "--panel_names_gms", action="store_true",
-        help="Write gms panel names in a file"
-    )
-    generate.add_argument(
-        "-pn_all", "--panel_names_all", action="store_true",
-        help="Gemini database xls dump"
-    )
 
     check = subparser.add_parser("check")
     check.add_argument(
-        "-g", "--gene",
-        help="Get transcripts of given gene using the nirvana GFF"
-    )
-    check.add_argument(
-        "-p", "--panel",
+        "folder", nargs="+",
         help=(
             "Folder containing panel dump and "
             "check if the panel data imported matches the panelapp dumps"
         )
-    )
-    check.add_argument(
-        "-t", "--test", action="store_true",
-        help="Test that the tests has been correctly imported in the db"
-    )
-
-    query = subparser.add_parser("query")
-    query.add_argument(
-        "-g", "--gemini_name",
-        help="Query to get gemini name from part of the gemini name"
-    )
-    query.add_argument(
-        "-t", "--gene_test",
-        help="Query the genes in a test code"
     )
 
     mod_db = subparser.add_parser("mod_db")
