a9a5d4979d2eda2ad04eab778ce7d017b6e91aa0
Yu-jinKim <yujin.kim@hotmail.fr>
2021-01-21 11:22:15 +0000

    thought i had change that in the merge

--- ops/config.py
+++ ops/config.py
@@ -1,8 +1,8 @@
 # Path to panel logs folder
-path_to_logs = "/home/kimy/NHS/Panelapp/panel_logs"
+path_to_logs = "/home/egg-user/panels/panel_logs"
 
 # Path to the panel_palace folder
-path_to_panel_palace = "/home/kimy/NHS/Panelapp/panel_palace/"
+path_to_panel_palace = "/home/egg-user/panels/panel_palace/"
 
 # Path to the panel_config folder
-path_to_panel_config = "/home/kimy/NHS/Panelapp/panel_config"
+path_to_panel_config = "/home/egg-user/panels/panel_config"
