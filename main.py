4740ffc0355c9ca3c79faaf691e73974b14be3e5
Yu-jinKim <yujin.kim@hotmail.fr>
2021-02-03 15:24:09 +0000

    added hgnc import, g2t, changes to the json generate, check

--- main.py
+++ main.py
@@ -4,16 +4,18 @@
 
 3 subparsers:
 - check:
-    - panel: check the panelapp dump folder given against the database
+    - panel: Check the panelapp dump folder given against the database
 - generate:
-    - panelapp_gms: generate panelapp dump using only GMS panels
-    - panelapp_all: generate panelapp dump using only all panels
-    - gene_files: generate files for panels with gene symbols only
-    - json: generate django fixture using panelapp dump folder
-    - genepanels: generate genepanels file
-    - manifest: generate manifest type file for reports using Gemini db dump
+    - panelapp_gms: Generate panelapp dump using only GMS panels
+    - panelapp_all: Generate panelapp dump using only all panels
+    - gene_files: Generate files for panels with gene symbols only
+    - json: Generate django fixture using panelapp dump folder
+    - genepanels: Generate genepanels file
+    - manifest: Generate manifest type file for reports using Gemini db dump
+    - g2t: Generate genes2transcript file
 - mod_db:
     - initial_import: Import given django fixture in the database
+    - hgnc: Import HGNC data dump in the database
 """
 
 import argparse
@@ -27,29 +29,47 @@ import config_panel_db
 
 
 def main(**param):
-    # gather data from the test directory
-    clinind_data = ops.utils.parse_test_directory(param["test_xls"])
-    clean_clinind_data = ops.utils.clean_targets(clinind_data)
-
-    # get the single genes in the test directory
-    single_genes = ops.utils.gather_single_genes(clean_clinind_data)
-
-    user = config_panel_db.user_admin
-    passwd = config_panel_db.passwd_admin
+    user = config_panel_db.user_ro
+    passwd = config_panel_db.passwd_ro
     host = config_panel_db.host
 
+    if param["test_xls"]:
+        # gather data from the test directory
+        clinind_data = ops.utils.parse_test_directory(param["test_xls"])
+        clean_clinind_data = ops.utils.clean_targets(clinind_data)
+
+        # get the single genes in the test directory
+        single_genes = ops.utils.gather_single_genes(clean_clinind_data)
+
     if param["command"] == "check":
+        assert clean_clinind_data is not None, (
+            "-t option is needed for check cmd"
+        )
         # Check integrity of database for all things panel
-        if param["folder"]:
-            session, meta = ops.utils.connect_to_db(user, passwd, host)
+        if param["dumps"]:
+            files = {
+                ele.split("=")[0]: ele.split("=")[1]
+                for ele in param["dumps"]
+            }
+
+            session, meta = ops.utils.connect_to_db(
+                user, passwd, host, "panel_database"
+            )
             (
                 panelapp_dict, superpanel_dict, gene_dict
             ) = ops.utils.create_panelapp_dict(
-                param["folder"], config_panel_db.panel_types, single_genes
+                files["panelapp"].split(";"), config_panel_db.panel_types,
+                single_genes
+            )
+            (
+                hgnc_data, symbol_dict, alias_dict, prev_dict
+            ) = ops.utils.parse_hgnc_dump(files["hgnc"])
+            nirvana_data = ops.utils.get_nirvana_data_dict(
+                files["nirvana"], symbol_dict, alias_dict, prev_dict
             )
             check = ops.check.check_db(
-                param["folder"], session, meta, panelapp_dict, superpanel_dict,
-                clean_clinind_data
+                files, session, meta, panelapp_dict, superpanel_dict,
+                gene_dict, nirvana_data, hgnc_data, clean_clinind_data
             )
 
     elif param["command"] == "generate":
@@ -76,85 +96,78 @@ def main(**param):
 
         # Generate a bioinformatic manifest type file for reports
         if param["manifest"]:
-            session, meta = ops.utils.connect_to_db(user, passwd, host)
+            session, meta = ops.utils.connect_to_db(
+                user, passwd, host, "panel_database"
+            )
             sample2panels = ops.generate.generate_manifest(
                 session, meta, param["manifest"]
             )
 
         # Generate django fixture using given panelapp dump
         if param["json"]:
+            assert clean_clinind_data is not None, (
+                "-t option is needed for generate json cmd"
+            )
+            files = {
+                ele.split("=")[0]: ele.split("=")[1]
+                for ele in param["json"]
+            }
+
             # Primary keys for importing the data in the database
             pk_dict = {
                 # they start at 1 because I loop over those elements
                 # makes it easier to keep track off
-                "clinind": 1, "panel": 1, "reference": 1,
+                "clinind": 0, "panel": 0, "reference": 0, "feature_type": 0,
                 "clinind_panels": 0, "feature": 0, "panel_feature": 0,
-                "gene": 0, "str": 0, "cnv": 0, "panel_type": 1
+                "gene": 0, "panel_type": 0, "transcript": 0, "g2t": 0
             }
 
             (
-                panelapp_dict, superpanel_dict, gene_dict
-            ) = ops.utils.create_panelapp_dict(
-                param["json"], config_panel_db.panel_types, single_genes
-            )
-
-            # Create the list of reference table
-            reference_json = ops.utils.gather_ref_django_json(
-                config_panel_db.references
-            )
-            # Create the list of panel_type table
-            paneltype_json = ops.utils.gather_panel_types_django_json(
-                config_panel_db.panel_types
-            )
-            # Create the list of feature_type table
-            featuretype_json = ops.utils.gather_feature_types_django_json(
-                config_panel_db.feature_types
-            )
-
-            # Create the list for data associated with panels
-            (
-                panel_json, feature_json, panelfeature_json, featuretype_json,
-                gene_json, pk_dict
-            ) = ops.utils.gather_panel_data_django_json(
-                panelapp_dict, gene_dict, featuretype_json, paneltype_json,
-                reference_json, pk_dict
-            )
-
-            # Add the superpanels to the list of panel objects
-            (
-                panel_json, panelfeature_json
-            ) = ops.utils.gather_superpanel_data_django_json(
-                superpanel_dict, panel_json, paneltype_json, reference_json,
-                panelfeature_json, pk_dict
-            )
-
-            # Create the list of clinical indication
-            (
-                clinical_indication_json, clinical_indication2panels_json
-            ) = ops.utils.gather_clinical_indication_data_django_json(
-                clean_clinind_data, panel_json, pk_dict
+                hgnc_data, symbol_dict, alias_dict, prev_dict
+            ) = ops.utils.parse_hgnc_dump(files["hgnc"])
+            nirvana_data = ops.utils.get_nirvana_data_dict(
+                files["nirvana"], symbol_dict, alias_dict, prev_dict
             )
 
             # Generate the jsons for the import
             ops.generate.generate_django_jsons(
-                ref=reference_json, panel_type=paneltype_json,
-                feature_type=featuretype_json, panel_feature=panelfeature_json,
-                gene=gene_json, clinical_indication=clinical_indication_json,
-                feature=feature_json, panel=panel_json,
-                clinical_indication2panels=clinical_indication2panels_json
+                files["dump"].split(";"), clean_clinind_data, hgnc_data,
+                nirvana_data, single_genes, config_panel_db.references,
+                config_panel_db.feature_types, config_panel_db.panel_types,
+                pk_dict
             )
 
         # Generate genepanels file from database
         if param["genepanels"]:
-            session, meta = ops.utils.connect_to_db(user, passwd, host)
+            session, meta = ops.utils.connect_to_db(
+                user, passwd, host, "panel_database"
+            )
             ops.generate.generate_genepanels(session, meta)
 
-    elif param["command"] == "mod_db":
-        # Import given django fixture to the database
-        if param["initial_import"]:
-            ops.mod_db.import_django_fixture(
-                param["initial_import"]
+        # Generate genes2transcripts file from database
+        if param["g2t"]:
+            session, meta = ops.utils.connect_to_db(
+                user, passwd, host, "panel_database"
             )
+
+            ops.generate.generate_g2t(session, meta)
+
+    elif param["command"] == "mod_db":
+        if (
+            param["user"] == config_panel_db.user_admin and
+            param["passwd"] == config_panel_db.passwd_admin
+        ):
+            # Import given django fixture to the database
+            if param["initial_import"]:
+                ops.mod_db.import_django_fixture(param["initial_import"])
+
+            # Import HGNC dump file
+            if param["hgnc"]:
+                args = {
+                    ele.split("=")[0]: ele.split("=")[1]
+                    for ele in param["hgnc"]
+                }
+                ops.mod_db.import_hgnc_dump(args["hgnc"], args["date"])
 
 
 if __name__ == "__main__":
@@ -162,9 +175,9 @@ if __name__ == "__main__":
         description="Prepare and import data for Panelapp database"
     )
 
-    subparser = parser.add_subparsers(dest="command")
+    parser.add_argument("-t", "--test_xls", help="NHS test directory")
 
-    parser.add_argument("test_xls", help="NHS test directory")
+    subparser = parser.add_subparsers(dest="command")
 
     generate = subparser.add_parser("generate")
     generate.add_argument(
@@ -180,11 +193,21 @@ if __name__ == "__main__":
         help="Generate all panelapp dump"
     )
     generate.add_argument(
-        "-j", "--json", nargs="+",
+        "-j", "--json", metavar="KEY=VALUE", nargs=3,
         help=(
-            "Dump folders to generate json to import data directly, "
-            "must contain: gms, non_gms, in_house"
+            "Generate django json that need to be imported in the database. "
+            "Generating the jsons files require 3 files: "
+            "the panelapp dump(s)/custom dump(s) to actually import, "
+            "hgnc file, nirvana gff file. They need to be given as "
+            "following: \"dump=file;file, hgnc=file, nirvana=file\". "
+            "The panelapp dump file path should contain the following string: "
+            "gms, non-gms, in-house, single_gene. This allows to find the "
+            "type of panel the folder contains."
         )
+    )
+    generate.add_argument(
+        "-g2t", "--g2t", action="store_true",
+        help="Generate genes2transcripts file"
     )
     generate.add_argument(
         "-gp", "--genepanels", action="store_true",
@@ -194,17 +217,28 @@ if __name__ == "__main__":
 
     check = subparser.add_parser("check")
     check.add_argument(
-        "folder", nargs="+",
+        "dumps", metavar="KEY=VALUE", nargs=3,
         help=(
-            "Folder containing panel dump and "
-            "check if the panel data imported matches the panelapp dumps"
+            "Provide panelapp dump, hgnc dump and nirvana gff. The format for "
+            "passing those arguments is: panelapp=folder hgnc=file "
+            "nirvana=file."
         )
     )
 
     mod_db = subparser.add_parser("mod_db")
+    mod_db.add_argument("user", help="Admin username for panel_database")
+    mod_db.add_argument("passwd", help="Admin passwd for panel_database")
+
     mod_db.add_argument(
         "-i", "--initial_import",
         help="Import pointed json in the database"
+    )
+    mod_db.add_argument(
+        "-hgnc", "--hgnc", metavar="KEY=VALUE", nargs=2,
+        help=(
+            "Import hgnc dump in the database. Need to provide "
+            "hgnc=file date=yymmdd"
+        )
     )
 
     args = vars(parser.parse_args())
