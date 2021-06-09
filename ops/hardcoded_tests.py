d4c7385712ad3b783a531f23c0df5b032b1ef93b
Yu-jinKim <yujin.kim@hotmail.fr>
2021-06-09 10:32:22 +0100

    typo fix in R27.1a

--- ops/hardcoded_tests.py
+++ ops/hardcoded_tests.py
@@ -14,7 +14,7 @@ tests = {
     "R27.1": {
         "gemini_name": (
             "R27.1a_Congenital malformation and dysmorphism syndromes"
-            " - microarray and sequence (DDG2P)_P"
+            " - microarray and sequencing (DDG2P)_P"
         ),
         "panels": ["484"],
         "tests": []
